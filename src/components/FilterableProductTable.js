b17831e955a8de0dfc435f68d28800aa4a63f213
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-12-14 00:38:17 +0000

    Step 4: Identify Where Your State Should Live and Step 5: Add Inverse Data Flow done

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -7,13 +7,26 @@ class FilterableProductTable extends React.Component{
   constructor(props){
     super(props)
     this.state = {inStockOnly: false, filterText: ''}
+    this.onFilterTextChange = this.onFilterTextChange.bind(this);
+    this.onInStockChange = this.onInStockChange.bind(this);
   }
+
+  onFilterTextChange(filterText) {
+    this.setState({ filterText })
+  };
+
+  onInStockChange(inStockOnly){
+    this.setState({inStockOnly})
+  };
+
   render(){
     return (
       <div style={{"width": "50%", "margin": "auto", "marginTop": "100px"}}>
         <SearchBar
           inStockOnly={this.state.inStockOnly}
           filterText={this.state.filterText}
+          handleFilterTextChange={this.onFilterTextChange}
+          handleInStockChange={this.onInStockChange}
           />
         <ProductTable
           products={this.props.products}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange = (e) => {
+    this.props.handleFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange = (e) => {
+    this.props.handleInStockChange(e.target.checked);
+  }
+
   render() {
     return (
       <form>
         <input
           type= "text"
+          placeholder='Search...'
           value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
         />
         <p>
           <input
             id="inStock"
             type="checkbox"
             checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
           />
           <label htmlFor="inStock">Only show products in stock</label>
         </p>
